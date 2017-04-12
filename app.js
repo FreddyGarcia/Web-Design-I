9fd16508d8f983bbbfa8bd048f94b05d052651cb
Freddy Garcia Abreu <buscando_alink@hotmail.com>
2017-04-12 14:52:11 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,122 @@
+angular.module('MyApp', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider.state('home', {
+		url: '/home',
+		template: '<h1>Index</h1>'
+	});
+
+	$stateProvider.state('employees', {
+		url: '/employees',
+		templateUrl: 'employees/index.html',
+		controller : 'EmployeesController'
+	});
+	$stateProvider.state('projects', {
+		url: '/projects',
+		templateUrl: 'projects/index.html',
+		controller : 'ProjectosController'
+	});
+
+	$stateProvider.state('projectDetail', {
+		url: '/projects/:projectID/details',
+		templateUrl: 'projects/detail.html',
+		controller : 'ProjectDetailController'
+	});
+
+	$stateProvider.state('projectForm', {
+		url: '/projects/:projectID',
+		templateUrl: 'projects/form.html',
+		controller : 'ProjectFormController'
+	});
+})
+
+.service('MyService', function () {
+	this.projects = [
+		{
+			id  : 1,
+			name : 'Projecto 1',
+			comercialDen : 'KO',
+			status : '1',
+			tasks : [
+				{ name : 'Levantamiento' },
+				{ name : 'Documentacion' }
+			]
+		},
+		{
+			id  : 2,
+			name : 'Projecto 2',
+			comercialDen : 'KO',
+			status : '1',
+			tasks : [
+				{ name : 'Levantamiento' },
+				{ name : 'test' }
+			]
+		},
+		{
+			id  : 3,
+			name : 'Projecto 3',
+			comercialDen : 'KO',
+			status : '1',
+			tasks : [
+				{ name : 'Levantamiento' },
+				{ name : 'test' }
+			]
+		}
+	];
+
+	this.employees = [
+		{ id : 1, name : 'Eren Jaeger'},
+		{ id : 2, name : 'Mikasa Ackerman'},
+		{ id : 3, name : 'Armin Arlet'}
+	]
+})
+
+.factory('MyFactory', function (MyService) {
+	var obj = {};
+
+	obj.getProjectById = function (projectID) {
+
+		for (var i in MyService.projects) {
+			var project = MyService.projects[i];
+
+			if (project.id == projectID) {
+				return project;
+			}
+		}
+	}
+
+	return obj;
+})
+
+
+.run(function($rootScope) {
+    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    	$rootScope.state_name = toState.name;
+    });
+})
+
+.controller('EmployeesController', function ($scope, $state, MyService) {
+	$scope.state = $state.current.name;
+	$scope.employees = MyService.employees;
+})
+
+.controller('ProjectosController', function ($scope, $state, MyService) {
+	$scope.state = $state.current.name;
+	$scope.projects = MyService.projects;
+})
+
+.controller('ProjectDetailController', function ($scope, $stateParams, MyFactory) {
+	var projectID = $stateParams.projectID;
+	$scope.project = MyFactory.getProjectById(projectID);
+})
+
+.controller('ProjectFormController', function ($scope, $stateParams, MyFactory) {
+	$scope.project = {};
+	$scope.saveProject = function () {
+		console.log($scope.project);
+	}
+})
+
